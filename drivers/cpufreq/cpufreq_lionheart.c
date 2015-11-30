4d3c995e19e68e76f33e54a6fbe8bdcec5a760db
fechinator <fechter73@googlemail.com>
2015-11-30 22:47:58 +0100

    cpufreq: add missing eralysuspend.h for lionheart governor

--- drivers/cpufreq/cpufreq_lionheart.c
+++ drivers/cpufreq/cpufreq_lionheart.c
@@ -42,6 +42,7 @@
 #include <linux/input.h>
 #include <linux/workqueue.h>
 #include <linux/slab.h>
+#include <linux/earlysuspend.h>
 
 #define DEF_FREQUENCY_UP_THRESHOLD		(99)
 #define DEF_FREQUENCY_DOWN_THRESHOLD		(40)
